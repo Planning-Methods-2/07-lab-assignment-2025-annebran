5009871e782587a4943efb22923289b297dfe1df
Anne Bransford <anne.bransford@utsa.edu>
2025-05-07 12:06:14 -0500

    resubmitting

--- Lab7_Assignment.R
+++ Lab7_Assignment.R
@@ -5,24 +5,85 @@
 
 #---- Instructions ----
 
-# 1. [70 points] Open the R file "Lab7_Assignment.R" and answer the questions bellow
+# 1. [70 points] Open the R file "Lab7_Assignment.R" and answer the questions below
 # 2. [30 points] Run a T-test and an ANOVA test in your data.
 
 
-#---- Part 1. Open the R file "Lab7_Assignment.R" and answer the questions bellow
+#---- Part 1. Open the R file "Lab7_Assignment.R" and answer the questions below
 
 # 1.1 load the same household data used in the Lab7_Script.R file, create the object `HTS`
-
+hts <- data.table(read.spss("datasets/HTS.household.10regions.sav",to.data.frame = T))
 
 # 2. Recreate the same steps used in the lab to run a T-test, but instead, consider the following:
 # 2.1 Run a T-Test to show if the household income means is statistically different between households living in single family residences or not (use the whole sample). Produce two pdfs, one with an histogram pdf plot, and another with the simulated hypothesis testing plot showing where the T-statistic falls. Provide a short interpretation of your results
 
+library(data.table)
+library(ggplot2)
+
+income<-t.test(hhincome ~ sf, data=hts, var.equal=F)
+income
+
+income_histogram<-ggplot(hts, aes(x=hhincome))+
+  geom_histogram() +
+  facet_wrap(~sf)
+ggsave("hhincome_histogram.pdf", income_histogram)
+
+curve(dt(x, df=income$parameter), from =-40, to =40)
+abline(h=0, col='blue')
+points(x= income$statistic, y=0, col='red')
+upper975 <- qt(p = .975, df = income$parameter)
+abline(v = upper975,col='red')
+lower025 <- qt(p = .025, df = income$parameter)
+abline(v = lower025,col='red')
+
+# The t-test shows a significant difference in the mean household incomes between single family households and not single family households: t = -36.23, p-value < 2.2e-16. Because the p-value is less than .05 and t is negative, we reject the null hypothesis and conclude that single-family households tend to have significantly higher incomes than those in other types of housing.
+
+
 # 2.2 Filter the sample to select only the region of San Antonio. Prepare an T-Test to show if the household vehicle miles traveled (in natural logs - lnvmt) is statistically different between households living in neighborhoods with a job-population index (variable `jobpop`) over and under the city median (of the `jobpop` variable of course)
+
+sa_hts<-hts[region%in%c("San Antonio, TX")]
+median_jobpop <- median(sa_hts$jobpop, na.rm = TRUE)
+sa_hts$jobpop_group <- ifelse(sa_hts$jobpop > median_jobpop, "Above Median", "Below Median")
+
+lnvmt_ttest <- t.test(lnvmt ~ jobpop_group, data = sa_hts, var.equal = F)
+lnvmt_ttest
+
+#Since lnvmt_ttest calculates a p-value of .02759, which is less than .05, we reject the null hypothesis; since t = -2.2052, which is negative, we conclude that households in neighborhoods with a jobpop index below the city median tend to have significantly higher household vehicle miles traveled. 
 
 # 2.2 using the same data set (San Antonio sample), run an ANOVA test to see if there are significant differences between income categories and vehicle miles traveled by household. Follow the same steps used in the ANOVA exercise done in class. Produce three pdfs: one checking the normality assumption of the dependent variable, a second one checking the presence of outliers, and a third one showing the Tukey (post hoc) T-tests plot.
 
-# 2. [30 points] Run a T-test and an ANOVA test in your data.
+normality_histogram<-ggplot(sa_hts, aes(x=lnvmt)) +
+  geom_histogram()
+normality_histogram
 
+outliersplot<-ggplot(data=sa_hts, aes(x=income_cat, y=lnvmt)) +
+  geom_boxplot()
+outliersplot
+ggsave("outliersplot.pdf",plot=outliersplot)
+
+sa_hts_bp<-boxplot(sa_hts$lnvmt~sa_hts$income_cat)
+outliers<-sa_hts_bp$out
+sa_hts[lnvmt%in%outliers,]
+sa_hts2<-sa_hts[!lnvmt%in%outliers,]
+
+boxplot(sa_hts$lnvmt~sa_hts$income_cat)
+boxplot(sa_hts2$lnvmt~sa_hts2$income_cat)
+
+no_outliersplot<-ggplot(sa_hts2, aes(x=income_cat, y=lnvmt))+
+  geom_boxplot()
+no_outliersplot
+ggsave("no_outliersplot.pdf",plot=no_outliersplot)
+
+normality_histogram2<-ggplot(sa_hts2, aes(x=lnvmt))+
+  geom_histogram()
+normality_histogram2
+ggsave("notquitenormalbutbetterthanbefore.pdf",plot=normality_histogram2)
+
+anova<-aov(lnvmt~income_cat, data=sa_hts2)
+summary(anova)
+
+tukey<-TukeyHSD(anova)
+tukey_plot<-plot(tukey)
 
 # Bonus: [30 points] Provide an HTML file with your answers using R-Markdown.
 
--- Lab7_Script.R
+++ Lab7_Script.R
@@ -18,44 +18,44 @@
  ## 4. Conduct an T-test for independent samples
  ## 5. Interpret results
 
-library(data.table)
-library(foreign)
+library(data.table) #loads the pacakge data.table
+library(foreign) #loads the package foreign
 
-hts <- data.table(read.spss("datasets/HTS.household.10regions.sav",to.data.frame = T))
+hts <- data.table(read.spss("datasets/HTS.household.10regions.sav",to.data.frame = T)) #reads the SPSS file as a data frame and puts it in a data table format
 
 # step 1 - Independence: there is no reason to think that the VMT values between these two regions are not independent
-hts[,.N,by=region]
-hts<-hts[region%in%c("Seattle, WA","Kansas City, MO")]
+hts[,.N,by=region] #counts the number of observations for each region
+hts<-hts[region%in%c("Seattle, WA","Kansas City, MO")] #filters the data to include only Seattle and KC
 
 # step 1 - Normality: Histogram plots look pretty normal
 
-library(ggplot2)
+library(ggplot2) #loads the ggplot2 package
 
 ggplot(data=hts,aes(x=lnvmt))+
   geom_histogram()+
-  facet_grid(region~.)
+  facet_grid(region~.) #creates a histogram of values in the column lnvmt for each region in the plots tab
 
 
 # step 1 - Variance: Box-plot looks pretty normal
 
 ggplot(data=hts,aes(x=lnvmt, y=region))+
-  geom_boxplot()
+  geom_boxplot() #creates a boxplot of lnvmt for each region in the plots tab
 
  # desc stats
-hts[,.(mean=mean(lnvmt,na.rm=T),sd=sd(lnvmt,na.rm=T),Obs=.N),by=.(region)]
+hts[,.(mean=mean(lnvmt,na.rm=T),sd=sd(lnvmt,na.rm=T),Obs=.N),by=.(region)] #calculates the mean, standard deviation, and number of observations for the values in lnvmt for each region and prints it in the console
 
 
 #Step 2: dependent variable types
-class(hts$lnvmt) ; class(hts$region)
+class(hts$lnvmt) ; class(hts$region) #checks the datatypes of columns lnvmt and region in the object hts and prints them in the console
 
 # Step 3: H0: t-statistic = 0 (Mean_Seattle - Mean_Kansas = 0)
 
 # Step 4: Conduct t-test
 two_tailed_t_test<-hts[,t.test(formula = lnvmt ~ region)] # two-tailed
-two_tailed_t_test
+two_tailed_t_test #runs a two-tailed t test to compare lnvmt in the regions; creates a list object  in environment and then prints results in console
 
-one_tailed_t_test<-hts[,t.test(formula = lnvmt ~ region,alernative = 'greater')] # one-tailed
-one_tailed_t_test
+one_tailed_t_test<-hts[,t.test(formula = lnvmt ~ region,alternative = 'greater')] # one-tailed
+one_tailed_t_test #runs a one-tailed t test to see if lnvmt in one region is greater than the other; creates a list object in environment and then prints results in console
 
 # Step 5. Interpret results
 
@@ -65,13 +65,16 @@ one_tailed_t_test
 curve(dt(x, df = 5934.7), from = -10, to = 10)
 abline(h=0,col='blue')
 points(x=two_tailed_t_test$statistic,y=0,col='red')
+#plots the distribution of t with 5934.7 degrees of freedom (df value calculated from previous t-tests), drawns a blue line at y=0, and marks in red the point where the t values from the two tailed test are on the plot
 
 ##upper value from Chi-Squared Dist (1-alpha) with alpha=0.05
 upper975 <- qt(p = .975, df = 5934.7)
 abline(v = upper975,y=0,col='red')
+#calcs the 97.5% quantile of the distribution and draws a red line there
 
 lower025 <- qt(p = .025, df = 5934.7)
 abline(v = lower025,y=0,col='red')
+#calcs the bottom 2.5% quantile of the distribution and draws a red line there
 
 
 #---- 2. Learn to apply a ANOVA test (F-test) ----
@@ -86,7 +89,7 @@ abline(v = lower025,y=0,col='red')
  ## 6. Conduct post-hoc tests
 
 
-uza <- data.table(read.spss("datasets/UZA.sav",to.data.frame = TRUE))
+uza <- data.table(read.spss("datasets/UZA.sav",to.data.frame = TRUE)) #creates object uza from spss file brought in as data table
 
 
 # Step 1: Independence: There is no reason to think that TPM and region are dependent
@@ -94,41 +97,43 @@ uza <- data.table(read.spss("datasets/UZA.sav",to.data.frame = TRUE))
 
 ggplot(data=uza[,.(Mean_lntmp=mean(lntpm)),by=region], aes(x=region,y=Mean_lntmp))+
   geom_point()
+#creates plot of the mean lntmp for each region 
 
-
-# Step 1: to many outliers?: There is no reason to think that TPM and region are dependent
+# Step 1: too many outliers?: There is no reason to think that TPM and region are dependent
   #box plot shows that there are some outliers in the Midwest and South regions. Although using the LN transformation helps here, lets delete those observations)
 
 ggplot(data=uza, aes(x=region, y= lntpm))+
   geom_boxplot()
+#creates a boxplot for lntpm for each region and shows where the outliers are
   
   # deleting outliers 
-uza_bp<-boxplot(uza$lntpm~uza$region)
+uza_bp<-boxplot(uza$lntpm~uza$region) #turns the boxplot of lntpm by region into object uza_bp 
 
-outliers <- uza_bp$out
+outliers <- uza_bp$out #creates object outliers to contain the values in list "out" from usa_bp object
 
-uza[lntpm%in%outliers,]
-uza2<-uza[!lntpm%in%outliers,]
+uza[lntpm%in%outliers,] #filters to match the values in uza lntpm to the values in object outliers
+uza2<-uza[!lntpm%in%outliers,] #creates new object uza2 and removes the outlier values
 
-boxplot(uza$lntpm~uza$region)
-boxplot(uza2$lntpm~uza2$region)
+boxplot(uza$lntpm~uza$region) #boxplot with outliers
+boxplot(uza2$lntpm~uza2$region) #boxplot without outliers (but the low one from the south is still there?)
 
 # Step 1: dependent variable is normal: 
 
 hist(uza2$lntpm) # looks pretty normal to me!
+#plots a histogram of the column lntpm in object uza2
 
 # Step 1: variance homogeneity?
 bartlett.test(lntpm ~ region, data=uza2) # H0: variances are equal: p-value ==> accepts
-
+#runs a bartlett test in console to check if variances are equal across groups
 
 # Step 4: one-way anova
 
-fit<-aov(uza2$lntpm~uza2$region)
-summary(fit)
+fit<-aov(uza2$lntpm~uza2$region) #creates object fit containing results of anova test comparing the lntpm values in each region
+summary(fit) #prints a summary of the object in the console
 
 #post-hoc test
-TukeyHSD(fit)
+TukeyHSD(fit) #runs tukey's honest significant difference test to isolate which specific groups have the least/greatest differences
 
-plot(TukeyHSD(fit))
+plot(TukeyHSD(fit)) #plots the results of TukeyHSD to compare differences in each pair-up of regions
 
 
